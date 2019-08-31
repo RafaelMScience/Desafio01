33ee0350b25660efe5d5f76d6479d507588ec0ec
RafaelM <RafaelM.Science@gmail.com>
2019-08-31 16:29:57 -0400

    feat post
    - foi adicionado a funcao post para criacao do projeto

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
+
+const project = [];
+const task = [];
+
+server.post("/projects", (req, res) => {
+  const { id, title, task } = req.body;
+
+  project.push(id, title, task);
+
+  return res.json(project);
+});
+
+server.listen(3000);
