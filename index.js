fe02f0338841d183af544d8f9d6d458a229535bf
RafaelMScience <46762611+RafaelMScience@users.noreply.github.com>
2019-09-01 18:00:48 -0400

    Revert "Merge pull request #2 from RafaelMScience/Feature-Get"
    This reverts commit cc005c668e9b8f5df643fa04d71bb19ad5a612e4.

--- index.js
+++ index.js
@@ -5,31 +5,11 @@ const server = express();
 server.use(express.json());
 
 const project = [];
-let cont = 0;
-
-function checkExist(req, res, next) {
-  const { id } = project[req.params.id];
-
-  if (project[id]) {
-    return res.status(400).json({ erro: "id ja existe" });
-  }
-
-  return next();
-}
-
-server.get("/projects", (req, res) => {
-  return res.json(project);
-});
 
 server.post("/projects", (req, res) => {
-  const { id, title } = req.body;
+  const [id] = req.body;
 
-  const project_desc = {
-    id,
-    title,
-    task: []
-  };
-  project.push(project_desc);
+  project.push(id);
 
   return res.json(project);
 });
